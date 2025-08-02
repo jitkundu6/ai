8215ad2b93af6789733f43e27cbb82a09d064d2e
Subhajit Kundu <jitkundu6@gmail.com>
2025-08-02 22:04:19 +0530

    Added FAISS news research tool

---
+++ langchain_basics/6_streamlit_agent/news_research_tool.py
@@ -0,0 +1,95 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import AzureChatOpenAI
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env
+load_dotenv()
+
+openai_key = os.getenv("OPENAI_API_KEY")
+openai_api_base = os.getenv("OPENAI_API_BASE")
+openai_api_version = "2025-01-01-preview"
+deployment_name = "gpt-4.1"
+# deployment_name = "gpt-4o"
+
+
+# llm = OpenAI(temperature=0.9, max_tokens=500)
+llm = AzureChatOpenAI(
+    openai_api_key=openai_key,
+    openai_api_base=openai_api_base,
+    deployment_name=deployment_name,
+    openai_api_version=openai_api_version,  # Or your specific version
+    temperature=0.0,
+    max_tokens=1000,
+    max_retries=3
+)
+
+st.title("News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = "faiss_store_openai.pkl"
+
+main_placeholder = st.empty()
+
+if process_url_clicked:
+    # load data
+    loader = UnstructuredURLLoader(urls=urls, metadata=("sources","urls"))
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2") # Using HuggingFace embeddings
+    vectorstore_huggingface = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    # Save the FAISS index to a pickle file
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore_huggingface, f)
+
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            urls = result.get("urls", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
+            if urls:
+                st.subheader("URLs:")
+                urls_list = urls.split("\n")
+                for url in urls_list:
+                    st.write(url)
